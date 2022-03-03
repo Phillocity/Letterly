8c28c33d9e75873dc079f24ee79e7fc77813f613
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-03-03 14:23:49 +0000

    Added form for generating a letter, but no styling

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,4 +4,22 @@ class LettersController < ApplicationController
     @letter = Letter.new
   end
 
+  def create
+    @letter = Letter.new(letter_params)
+    @letter.inbox_id = current_inbox.id
+    if @letter.save
+      redirect_to @users, notice: "Your letter was posted."
+    else
+      render :new, notice: "Something went wrong. Try again."
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:letter).permit(:content, :subject)
+  end
 end
--- app/models/letter.rb
+++ app/models/letter.rb
@@ -2,4 +2,6 @@ class Letter < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
   belongs_to :inbox
+
+  has_rich_text :content
 end
