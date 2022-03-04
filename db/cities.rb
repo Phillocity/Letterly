d8553cdaa1a325aac97df8c43c96ceffbc43a258
phillip burns <phillipworld@hotmail.co.uk>
2022-03-04 15:37:38 +0000

    Added button to homepage

--- db/cities.rb
+++ db/cities.rb
@@ -1,73 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-require 'json'
-require "open-uri"
-require_relative "cities"
-
-
-Letter.destroy_all
-Inbox.destroy_all
-User.destroy_all
-
-randomizer = ('a'..'z').to_a
-
-# def random_city
-#   url = "https://countriesnow.space/api/v0.1/countries"
-#   response = RestClient.get(url)
-#   puts response
-#   cities = []
-#   array = JSON.parse(response)["data"]
-#   array.each do |item|
-#     item["cities"].each do |city|
-#       cities << city
-#     end
-#   end
-#   cities.sample
-# end
-
-20.times do
-
-  user = User.new(name:Faker::Name.unique.name,
-                  email:Faker::Internet.email,
-                  gender:Faker::Gender.type,
-                  avatar: "https://avatars.dicebear.com/api/bottts/#{randomizer.sample(rand(1..26)).join}.svg" ,
-                  password:"111111",
-                  address: Cities::CITIES.sample,
-                  description:Faker::Quote.most_interesting_man_in_the_world,
-                  age:rand(18..40))
-
-  user1 = User.new(name:Faker::Name.unique.name,
-                  email:Faker::Internet.email,
-                  gender:Faker::Gender.type,
-                  avatar: "https://avatars.dicebear.com/api/bottts/#{randomizer.sample(rand(1..26)).join}.svg" ,
-                  password:"111111",
-                  address: Cities::CITIES.sample,
-                  description:Faker::Quote.most_interesting_man_in_the_world,
-                  age:rand(18..40))
-
-  user.save!
-  user1.save!
-
-  inbox = Inbox.new(first_user:user, second_user: user1)
-  inbox.save!
-
-  rand(5..15).times do
-    letter = Letter.new(sender: user, receiver: user1, inbox: inbox, content: Faker::TvShows::BojackHorseman.quote, subject: Faker::TvShows::BojackHorseman.character)
-    puts "[#{letter.subject}]\t \t Letter.sent!"
-    letter.save!
-
-    letter = Letter.new(sender:user1, receiver: user, inbox: inbox,  content: Faker::TvShows::BojackHorseman.quote, subject: Faker::TvShows::BojackHorseman.character)
-    puts "[#{letter.subject}]\t \t Letter sent!"
-    letter.save!
-  end
-end
-
+module Cities
 CITIES = [
 "A Coruña",
 "Aachen",
@@ -1837,3 +1768,4 @@ CITIES = [
 "Zwickau",
 "Zwolle",
 ]
+end
