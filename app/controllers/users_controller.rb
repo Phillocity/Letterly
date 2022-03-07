c519e732041f3a6d7a46af9065c4f263ed33d5a8
phillip burns <phillipworld@hotmail.co.uk>
2022-03-07 17:51:26 +0000

    initial attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,23 @@ class UsersController < ApplicationController
   def index
 
     if params[:query].present?
-      @users = User.search_user(params[:query])
+      @users = User.search_user(params[:query]).order('RANDOM()').first(5)
+      @markers = @users.map do |user|
+        {
+          lat: user.latitude,
+          lng: user.longitude,
+          # info_window: render_to_string(partial: "info_window", locals: { user: user }),
+          image_url: helpers.asset_url("marker.png")
+        }
+      end
     end
 
     @users = User.order('RANDOM()').first(5)
-    @users_sample = []
-    @markers = User.geocoded.map do |user|
-      puts user
-      @users_sample << user
+    @markers = @users.map do |user|
       {
         lat: user.latitude,
         lng: user.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { user: user }),
+        # info_window: render_to_string(partial: "info_window", locals: { user: user }),
         image_url: helpers.asset_url("marker.png")
       }
     end
