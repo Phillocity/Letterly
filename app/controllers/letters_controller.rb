ea840ff3c7a7ab8c23d33f62c5a09149457cd777
phillip burns <phillipworld@hotmail.co.uk>
2022-03-09 14:49:25 +0000

    Arrival time logic

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -12,7 +12,8 @@ class LettersController < ApplicationController
     @letter.sender_id = current_user.id
     @letter.receiver_id = @inbox.first_user_id == current_user.id ? @inbox.second_user_id : @inbox.first_user_id
     @letter.delivery_time = delivery_in_seconds(@letter.sender, @letter.receiver)
-    @letter.arrival_time = (Time.now + @letter.delivery_time).at_beginning_of_hour
+    @letter.arrival_time = (DateTime.now + @letter.delivery_time.seconds).at_beginning_of_hour
+
     if @letter.save!
       redirect_to pals_path, notice: "Your letter is being sent."
     else
@@ -29,13 +30,18 @@ class LettersController < ApplicationController
     params.require(:letter).permit(:content, :subject)
   end
 
+  def distance(user1, user2)
+    Geocoder::Calculations.distance_between(user1, user2)
+  end
+
   def delivery_in_seconds(user1, user2)
     user1 = user1.geocode
     user2 = user2.geocode
 
-    distance = Geocoder::Calculations.distance_between(user1, user2)
-    miles_per_hour = 80
+    distance = distance(user1, user2)
+    miles_per_hour = 100
     delivery_time_in_seconds = (distance / miles_per_hour) * 3600
     return delivery_time_in_seconds.to_i
   end
+
 end
