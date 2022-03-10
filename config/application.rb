0a04ac39897ce8e324bc131687b067dd61d4ab1b
phillip burns <phillipworld@hotmail.co.uk>
2022-03-10 14:37:56 +0000

    fix arrival time

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative "boot"
 require "rails/all"
+require 'active_support'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
