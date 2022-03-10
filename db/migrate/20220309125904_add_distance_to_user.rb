fb9ae823d8852ed6e041b3cb3e8cf43873e565fa
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-03-10 17:37:51 +0000

    pop_ups for home page

--- db/migrate/20220309125904_add_distance_to_user.rb
+++ db/migrate/20220309125904_add_distance_to_user.rb
@@ -1,5 +1,5 @@
 class AddDistanceToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :distance_from, :integer
+    add_column :users, :letter_distance_from, :integer
   end
 end
