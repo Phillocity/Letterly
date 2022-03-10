8239891310407c0656eb97f275de65a7392b67da
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-03-10 17:02:27 +0000

    fixed schema issues

--- db/schema.rb
+++ db/schema.rb
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_125904) do
     t.float "longitude"
     t.text "avatar"
     t.string "gender"
-    t.integer "distance_from"
+    t.integer "letter_distance_from"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
