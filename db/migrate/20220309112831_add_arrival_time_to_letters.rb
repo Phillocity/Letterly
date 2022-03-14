78595b2aea09eb1c91283dac19a4de0b94dcbe65
phillip burns <phillipworld@hotmail.co.uk>
2022-03-14 12:00:08 +0000

    testing date change in schema

--- db/migrate/20220309112831_add_arrival_time_to_letters.rb
+++
@@ -1,5 +0,0 @@
-class AddArrivalTimeToLetters < ActiveRecord::Migration[6.1]
-  def change
-    add_column :letters, :arrival_time, :time
-  end
-end
--- db/migrate/20220309125904_add_distance_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDistanceToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :letter_distance_from, :integer
-  end
-end
