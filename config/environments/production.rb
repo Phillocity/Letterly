a5261251b56283178e417cd1d86f1774e471eb38
phillip burns <phillipworld@hotmail.co.uk>
2022-03-14 10:04:45 +0000

    Change in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  Rails.logger.level = 0 # at any time
 end
-
-Rails.logger.level = 0 # at any time
